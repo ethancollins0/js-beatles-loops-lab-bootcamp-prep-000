ff126c4c6cbca4dc1a5f3075bd7f848480ebc859
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 16:29:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var beatlesArray = []
+  for (var i = 0; i < musicians.length; i++) {
+    beatlesArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatlesArray
+}
+
+function johnLennonFacts(facts) {
+  var factsArray = []
+  var i = 0;
+  while (i < facts.length) {
+      factsArray.push(`${facts[i]}!!!`)
+      i++;
+  }
+  return factsArray
+}
